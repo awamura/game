1f22d981ba20d078c1cfa1573c681ae897eecf1e
awamura_yuki <yuyumooon@gmail.com>
2014-11-23 15:00:56 +0900

    no message

---
+++ Kadai/Assets/Chimney.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	GameObject gameController;
+	
+	void Start () {
+		gameController = GameObject.Find ("GameController");
+	}
+
+	void OnCollisionEnter(Collision collisionInfo) {
+		if (collisionInfo.gameObject.tag == "character") {
+			gameController.SendMessage("GameClear");
+		}
+	}
+}
---
+++ Kadai/Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GUITexture texClear;
+	public GUITexture texFaild;
+	
+	private bool isStageEnd;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GameClear() {
+		texClear.enabled = true;
+		isStageEnd = true;
+	}
+
+	public void GameFaild() {
+		texFaild.enabled = true;
+		isStageEnd = true;
+	}
+}
