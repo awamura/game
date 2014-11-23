c5ffa138d4866af86a2e0ed8bcbf81da9eea4a8e
takuya_yanase <infinite.blue1632@gmail.com>
2014-11-23 14:53:32 +0900

    テディベアAsset追加

---
+++ Kadai/Assets/Stage/Scripts/ForceUp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceUp : MonoBehaviour {
+
+	public float power;
+
+	void OnCollisionEnter(Collision collision) {
+		GameObject targetObject = collision.gameObject;
+		targetObject.rigidbody.AddForce (Vector3.up.normalized * power, ForceMode.VelocityChange);
+		Debug.Log ("add force");
+	}
+}
