a124ba9a47e5b1803fec7c565b6d1b756ec37198
awamura_yuki <yuyumooon@gmail.com>
2014-11-23 13:43:32 +0900

    キャラの動き変更、サンタのアセット取り込み

--- Kadai/Assets/Character.cs
+++ Kadai/Assets/Character.cs
@@ -2,80 +2,65 @@
 using System.Collections;
 
 public class Character : MonoBehaviour {
-	public float SPEED = 0.1F;
-	public float ROTATE_SPEED = 1F;
+	public float SPEED = 1F;
+	public float ROTATE_SPEED = 2F;
+	public float GRAVITY = 200F;
+
+	Animator animator;
+	CharacterController character;
 
 	private float beforePositionZ;
-	private float v;
+	private float speedX;
 	private bool isRotateMode;
-	private float afterRotate;
+	private float beforeRotate;
+
+	Vector3 velocity;
 
 	//private bool isGround = false;
 
 	// Use this for initialization
 	void Start () {
 		beforePositionZ =  transform.position.z;
-		v = SPEED;
-
+		speedX = SPEED;
 		
-		afterRotate = transform.rotation.y + 1;
+		animator = GetComponent<Animator>();
+		character = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		print (Mathf.Abs(afterRotate - transform.rotation.y));
-//		if (Mathf.Abs (afterRotate - transform.rotation.y) > 0.01) {
-//			Vector3 angle = new Vector3 (0, ROTATE_SPEED, 0);
-//			transform.Rotate (angle);
-//		} else {
-//			Vector3 angle = new Vector3 (transform.rotation.x, transform.rotation.y + 1, transform.rotation.z);
-//		}
-
-//		isGround = true;
-//		if (isGround) {
-//			print ("false");
-//			isGround = false;
-//		} else {
-//			speed *= -1;
-//		}
-		
-		transform.Translate (0, 0, v);
-
-//		if (Mathf.Abs (v) > 0.001) {
-//			transform.Translate (0, 0, v);
-//		}
-
-		if (isRotateMode) {
-			if (Mathf.Abs (afterRotate - transform.rotation.z) > ROTATE_SPEED) {
-//				Vector3 angle = new Vector3(0, ROTATE_SPEED, 0);
-//				transform.Rotate(angle);
-				transform.rotation.Set(0,0, afterRotate,0);
-			} else {
-				transform.rotation.Set(0,0, afterRotate,0);
-				isRotateMode = false;
-				v = SPEED;
-			}
+		if (Mathf.Abs (speedX) > 0.001) {
+			velocity = new Vector3(0, 0, speedX);
+			animator.SetFloat("Speed", Mathf.Abs (speedX));
 		}
 
-		if (!isRotateMode && Mathf.Abs (beforePositionZ - transform.position.z) < SPEED * 0.5) {
-			startRorate();
+		velocity.y -= GRAVITY * Time.deltaTime;
+		CollisionFlags flag = character.Move(velocity * Time.deltaTime);
+		
+		if (isRotateMode) {
+			if (Mathf.Abs (beforeRotate - transform.rotation.y) < (1 - ROTATE_SPEED / 180)) {
+				Vector3 angle = new Vector3 (0, ROTATE_SPEED, 0);
+				transform.Rotate (angle);
+			} else {
+				transform.rotation = new Quaternion(0, Mathf.Round(transform.rotation.y), 0, 0);
+				print (transform.rotation.y);
+				isRotateMode = false;
+				speedX = -SPEED * (transform.rotation.y * 2 - 1);
+			}
+		} else {
+			if ((flag & CollisionFlags.Sides) == CollisionFlags.Sides) {
+				startRorate();
+			}
 		}
 		
 		beforePositionZ = transform.position.z;
 	}
 
 	private void startRorate() {
-		//isRotateMode = true;
+		isRotateMode = true;
 
-		//print (transform.rotation.z);
-		afterRotate = transform.rotation.y + 1;
-		//v = 0;
+		beforeRotate = transform.rotation.y;
+		speedX = 0;
+		animator.SetFloat("Speed", 0.1F);
 	}
-
-//	void OnCollisionStay(Collision collisionInfo) {
-//		if (collisionInfo.gameObject.tag == "ground") {
-//			print("true");
-//			isGround = true;
-//		}
-//	}
 }
