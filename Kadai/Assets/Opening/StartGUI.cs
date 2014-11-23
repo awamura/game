fe0fc642325a8a3fbbb1da4785dccdd91bb8449c
takuya_yanase <infinite.blue1632@gmail.com>
2014-11-23 16:44:50 +0900

    オープニング作成

---
+++ Kadai/Assets/Opening/StartGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGUI: MonoBehaviour {
+	
+	void OnGUI() {
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 50, 100, 50), "Start")) {
+			// クリックされたらStage01シーンをロードする
+			Application.LoadLevel ("Main");
+		}
+	}
+}
